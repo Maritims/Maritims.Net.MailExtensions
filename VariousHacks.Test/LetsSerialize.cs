b8c00ffa56a30303e99c87d95f2c126a0d96fb86
Martin Severin Steffensen <martin@novamanus.no>
2017-11-02 23:45:11 +0100

    Proof of concept for serializable mail messages based on System.Net.Mail.MailMessage

---
+++ VariousHacks.Test/LetsSerialize.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariousHacks.Tests
+{
+    public class LetsSerialize
+    {
+        [Test]
+        [TestCase("sender@example.com", "Jane Doe", "recipient@example.com", "John Doe", "A serializable message", "This message has been serialized and deserialized.")]
+        public void SerializableMailMessage(string sender, string senderName, string recipient, string recipientName, string subject, string body)
+        {
+            var mailMessage = new SerializableMailMessage
+            {
+                From = new MailAddress(sender, senderName)
+            };
+            mailMessage.To.Add(new MailAddress(recipient, recipientName));
+            mailMessage.Subject = subject;
+            mailMessage.Body = body;
+
+            var serializedData = JsonConvert.SerializeObject(mailMessage);
+            var deserializedData = JsonConvert.DeserializeObject<SerializableMailMessage>(serializedData);
+
+            Assert.AreEqual(sender, deserializedData.From.Address);
+            Assert.AreEqual(senderName, deserializedData.From.DisplayName);
+            Assert.AreEqual(recipient, deserializedData.To[0].Address);
+            Assert.AreEqual(subject, deserializedData.Subject);
+            Assert.AreEqual(body, deserializedData.Body);
+        }
+    }
+}
---
+++ VariousHacks.Test/SerializableMailMessage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariousHacks.Tests
+{
+    public class SerializableMailMessage : MailMessage, ISerializable
+    {
+        public SerializableMailMessage() : base()
+        {
+        }
+        
+        public SerializableMailMessage(SerializationInfo info, StreamingContext context) : base()
+        {
+            this.From = new MailAddress(info.GetString("sender"), info.GetString("senderName"));
+            this.Subject = info.GetString("subject");
+            this.Body = info.GetString("body");
+
+            Action<SerializationInfo, string> addRecipient = (serializationInfo, data) =>
+            {
+                var recipients = info.GetString(data);
+                if (string.IsNullOrEmpty(recipients))
+                    return;
+
+                recipients.Split(new string[] { ";" }, StringSplitOptions.None)
+                .Select(recipientAddress => new MailAddress(recipientAddress))
+                .ToList()
+                .ForEach(recipient =>
+                {
+                    if (data == "to")
+                        this.To.Add(recipient);
+                    else if (data == "cc")
+                        this.CC.Add(recipient);
+                    else if (data == "bcc")
+                        this.Bcc.Add(recipient);
+                });
+            };
+
+            addRecipient(info, "to");
+            addRecipient(info, "cc");
+            addRecipient(info, "bcc");
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("sender", this.From.Address);
+            info.AddValue("senderName", this.From.DisplayName);
+            info.AddValue("to", string.Join(";", this.To.Select(recipient => recipient.Address)));
+            info.AddValue("cc", string.Join(";", this.CC.Select(cc => cc.Address)));
+            info.AddValue("bcc", string.Join(";", this.Bcc.Select(bcc => bcc.Address)));
+            info.AddValue("subject", this.Subject);
+            info.AddValue("body", this.Body);
+        }
+    }
+}
